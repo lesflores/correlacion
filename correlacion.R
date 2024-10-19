6ee9adcdf98d11da4b1940d85d128e96e41bcd35
Lesly Estefanía Flores Rivera <147342430+lesflores@users.noreply.github.com>
2024-10-18 22:53:27 -0600

    Add files via upload

---
+++ correlacion.R
@@ -0,0 +1,18 @@
+# Tipos de correlación
+# Dependiendo del tipo de datos y la relación entre las variables.
+
+# Correlación de Pearson: 
+# Para variables continuas. 
+# Se asume que las variables tienen una distribución normal.
+cor(x, y, method = "pearson")
+
+# Correlación de Spearman: 
+# Para datos ordinales. 
+# Es una correlación no paramétrica.
+cor(x, y, method = "spearman")
+
+# Correlación de Kendall: 
+# Correlación no paramétrica.
+# Para los datos ordinales y pocos datos
+cor(x, y, method = "kendall")
+
